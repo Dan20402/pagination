a703ce30a20dcc5ee4d1a0fe9b80a9b9285550af
Marcelo DeCastro <marcelo@pi1.net>
2021-01-24 18:56:11 -0500

    fixed.

--- app2.js
+++ app2.js
@@ -1,21 +1,26 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
 const fetch = require('node-fetch');
 const buildUrl = require('build-url');
 
 const DEPUTADO_ID = 141439;
 
+const getNextLink = (links) => {
+    const nextLink = links.find((link) => link.rel === 'next');
+    return nextLink?.href;
+};
+
 const getFirstDespesas = async (deputadoId) => {
     const url = buildUrl('https://dadosabertos.camara.leg.br', {
         path: `/api/v2/deputados/${deputadoId}/despesas`,
         queryParams: {
-        ordem: 'ASC',
-        ordenarPor: 'ano',
+            ordem: 'ASC',
+            ordenarPor: 'ano',
         },
     });
-    console.log(url);
+
     const response = await fetch(url);
     const paginatedExpense = await response.json();
     return { paginatedExpense, nextLink: getNextLink(paginatedExpense.links) };
@@ -27,33 +32,22 @@ const getNextDespesas = async (url) => {
     return { paginatedExpense, nextLink: getNextLink(paginatedExpense.links) };
 };
 
-const getNextLink = (links) => {
-    console.log(links);
-    for (link of links) {
-        if (link.rel === 'next') {
-            return link.href;
-        }
-    }
-};
-
-
 const iterateResponse = async (response, allResponses) => {
-    
     if (!response.nextLink) {
+        console.log('EEEEEEEEEEEEEEEEND');
+        console.log(allResponses);
         return allResponses;
-    };
+    }
 
     const nextResponse = await getNextDespesas(response.nextLink);
-
-    iterateResponse(nextResponse, allResponses.concat(nextResponse.paginatedExpense.dados))
-}
+    return iterateResponse(nextResponse, allResponses?.concat(nextResponse.paginatedExpense.dados));
+};
 
 const main = async () => {
- 
     const response = await getFirstDespesas(DEPUTADO_ID);
     const firstResponse = response.paginatedExpense.dados;
 
-    console.log(JSON.stringify(firstResponse.concat(iterateResponse(response))));
-}
+    console.log(await iterateResponse(response, firstResponse));
+};
 
 main();
